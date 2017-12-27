8c6e309bee46c5245a40c64ef8b1e80f51eaae63
JosephAMumford <JosephAMumford@gmail.com>
2017-12-27 10:43:22 -0600

    initial setup

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.navbar-button').hover(function(){
+        $(this).css('background-color','rgb(112, 171, 194)');
+    }, function(){
+        $(this).css('background-color','skyblue');
+    })
+});
